7fb908758c3ed02fd9013025675c6d43648905f0
anan onda <anan.oasis.0719@gmail.com>
2015-10-30 19:28:56 +0900

    Initial Commit

---
+++ Orizinal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Orizinal
+//
+//  Created by onda anan on 2015/10/30.
+//  Copyright © 2015年 onda anan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
